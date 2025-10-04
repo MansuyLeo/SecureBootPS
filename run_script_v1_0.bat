356c93616c7d7e1c0916fa0309fd851bd95aa046
Mansuy Léo <169048834+MansuyLeo@users.noreply.github.com>
2025-10-04 16:16:15 +0200

    Update run_script_v1_0.bat

--- run_script_v1_0.bat
+++ run_script_v1_0.bat
@@ -1,4 +1,5 @@
-:: Script par Mansuy Léo - Alternant TAM PSL - DSNU DASU IDF - e.SNCF Solutions
+:: Script par Mansuy Léo
+:: Si modification de ce fichier batch, se rappeler de l'encoder en UTF-8
 :: Script batch pour forcer l'ouverture du script powershell avec la demande des droits administrateurs
 
 @echo off
@@ -16,13 +17,10 @@ echo ##############################################
 echo #                                            #
 echo #           SCRIPT SECUREBOOT v1.0           #
 echo #             Lenovo - HP - Dell             #
-echo #   Auteur: MANSUY Leo - Alternant TAM PSL   #
-echo #              e.SNCF Solutions              #
+echo #             Auteur: MANSUY Leo             #
 echo #                                            #
 echo ##############################################
 echo.
 
 :: Lancement du script PowerShell avec la demande des droits administrateurs (situé dans le même répertoire)
-PowerShell -NoLogo -ExecutionPolicy Bypass -NoExit -File "%~dp0SNCF_TAM_SecureBoot_v1_0.ps1"
-
-:: Si modification de ce fichier batch, se rappeler de l'encoder en UTF-8
+PowerShell -NoLogo -ExecutionPolicy Bypass -NoExit -File "%~dp0SecureBootPS_v1_0.ps1"
